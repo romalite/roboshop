fc05881273d9b93759accd1a6fe0990cc9325092
romalite <romalite32@gmail.com>
2021-12-18 19:52:13 +0200

    end

--- js_user.js
+++ js_user.js
@@ -6,6 +6,8 @@ const id = urlParams.get('id');
 let profile = 
 document.getElementById('profile');
 
+let product = document.getElementById('products')
+
 let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
 
 let userRequest = new XMLHttpRequest();
@@ -27,3 +29,31 @@ userRequest.onload = function() {
 }
 
 userRequest.send();
+
+let url1 = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace/products'
+let productsUser = new XMLHttpRequest()
+productsUser.open("GET",`${url}/products?author_id=${id}`)
+productsUser.responseType = 'json'
+productsUser.send()
+productsUser.onload =function () {
+	let products = productsUser.response
+	console.log(products)
+ 	product.innerHTML = `
+	
+	 <h1>${products[0].name}</h1>
+	<img class="products-img" 	
+	src=${products[0].photo_url}>
+	<h2>${products[0].price}</h2>
+	<h2>${products[0].description}</h2>
+	<div class="products2">
+
+	
+	<h3>${products[1].name}</h1>
+	<img class="products-img" 	
+	src=${products[1].photo_url}>
+	<h2>${products[1].price}</h2>
+	<h2>${products[1].description}</h2>
+	</div>
+	 
+	 `
+}
