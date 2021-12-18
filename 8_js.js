5fcc68baf706965dccb2a11554fe246606686091
romalite <93836569+romalite@users.noreply.github.com>
2021-12-18 18:30:13 +0200

    Add files via upload

---
+++ 8_js.js
@@ -0,0 +1,29 @@
+let productsGrid = document.getElementById('products-grid');
+let productsArray = [];
+let xhr = new XMLHttpRequest();
+let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+
+xhr.open('GET', url + '/products/');
+xhr.responseType = 'json';
+xhr.onload = function() {
+	let products = xhr.response;
+	productsGrid.innerHTML = null;
+
+	products.forEach(p => {
+		productsArray.push(p);
+		let pElem = document.createElement('div');
+		pElem.innerHTML = `
+		<div class="product">
+		<h2 class='product-name'>${p.name}</h2>
+		<img class='product-photo' src='${p.photo_url}'
+		alt='${p.name}'>
+		<p class='product-price'><b>Price: <b>${p.price}$</p>
+		<p class='product-description'><b>Description: </b>${p.description}</p>
+		<a href='index.html?id=${p.author_id}'>Seller profile</a>
+		<button>Buy</button>
+		</button>
+		`;
+		productsGrid.append(pElem);
+		});
+}
+xhr.send();
---
+++ js_user.js
@@ -0,0 +1,29 @@
+const urlParams = new
+URLSearchParams(window.location.search);
+
+const id = urlParams.get('id');
+
+let profile = 
+document.getElementById('profile');
+
+let url = 'https://my-json-server.typicode.com/RobocodeSchool/marketplace';
+
+let userRequest = new XMLHttpRequest();
+
+userRequest.open('GET', `${url}/users/${id}`);
+userRequest.responseType = 'json';
+userRequest.onload = function() {
+	let user = userRequest.response;
+	profile.innerHTML =
+	`
+	<h1>${user.name}</h1>
+	<h2>${user.sirname}</h2>
+
+	<img class="profile-img" 
+	src="${user.photo_url}">
+
+	<p>Balance: ${user.balance}$</p>
+	`
+}
+
+userRequest.send();
